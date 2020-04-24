9a3e393887449290d422ef18accd810fc317c57e
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2020-04-24 18:34:14 +0200

    Change blind docking to the cavity

--- tests/test_docking_py.py
+++ tests/test_docking_py.py
@@ -68,6 +68,18 @@ def test_smina_rigid(tmp_path, capsys):
     assert bool(re.match("Succeed to save file .+lig.pdb\n",
                 captured.out))
 
+    # Extract ligand center of mass and maximum dimension:
+    center_lig = lig_coor.center_of_mass()
+    max_size = lig_coor.get_max_size()
+    print("Center coordiantes is {:.1f} {:.1f} {:.1f}, maximum dimension"
+          " is {:.1f} Å".format(*center_lig, max_size))
+
+    captured = capsys.readouterr()
+    assert bool(re.match('Do a rotation of 99.65°\nCenter coordiantes is 13.1'
+                         ' 22.5 5.6, maximum dimension is 16.0 Å\n',
+                captured.out))
+
+
     # Create Docking object
     test_dock = docking_py.Docking('test',
                                    rec_pdb=out_rec,
@@ -113,32 +125,27 @@ def test_smina_rigid(tmp_path, capsys):
                           num_modes=10,
                           energy_range=1,
                           exhaustiveness=2,
-                          dock_bin='smina')
+                          dock_bin='smina',
+                          center=center_lig,
+                          grid_npts=[max_size + 10] * 3)
 
     captured = capsys.readouterr()
     capture_line = captured.out.split('\n')
     assert bool(re.match(
-        "Succeed to read file .+rec.pdb ,  1514 atoms found",
-        capture_line[0]))
-    assert "Grid points: [66 81 83]" == capture_line[1]
-    assert bool(re.match(
-        "Succeed to read file .+rec.pdb ,  1514 atoms found",
-        capture_line[2]))
-    assert bool(re.match(
         ("smina --ligand .+lig.pdbqt --receptor .+rec.pdbqt --log "
          ".+test_dock_log.txt --num_modes 10 --exhaustiveness 2"
          " --energy_range 1 --out .+test_dock.pdb "
-         "--size_x 66.00 --size_y 81.00 --size_z 83.00"
-         " --center_x 16.07 --center_y 26.49 --center_z 3.77"),
-        capture_line[3]))
+         "--size_x 26.00 --size_y 26.00 --size_z 26.00"
+         " --center_x 13.08 --center_y 22.49 --center_z 5.55"),
+        capture_line[0]))
 
     rmsd_list = test_dock.compute_dock_rmsd(test_dock.lig_pdbqt)
     assert len(rmsd_list) >= 1
-    assert rmsd_list[0] < 40
+    assert rmsd_list[0] < 5
 
     affinity = test_dock.extract_affinity()
     assert len(affinity) >= 1
-    assert affinity[1]['affinity'] < 0
+    assert affinity[1]['affinity'] < -10
 
 
 print("docking_py:    \t", doctest.testmod(docking_py))
