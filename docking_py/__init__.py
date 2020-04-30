4c7a4d47c45b4e625301a009e32db1942256e377
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2020-04-30 16:23:30 +0200

    Bump version: 0.2.1 → 0.2.2

--- docking_py/__init__.py
+++ docking_py/__init__.py
@@ -5,7 +5,7 @@ __author__ = "Samuel Murail, Damien Espana, Pierre Tuffery"
 __copyright__ = "Copyright 2020, RPBS"
 __credits__ = ["Damien Espana", "Pierre Tuffery", "Samuel Murail"]
 __license__ = "GNU General Public License v2.0"
-__version__ = "__version__ = '0.2.1'"
+__version__ = "__version__ = '0.2.2'"
 __maintainer__ = "Samuel Murail"
 __email__ = "samuel.murail@u-paris.fr"
 __status__ = "Prototype"
